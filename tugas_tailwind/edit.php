9dce3c3d012cf24b35088464d57e6e278a14773e
PIKACHA <140232108+putrinailah@users.noreply.github.com>
2023-09-02 11:45:03 +0700

    edit.php

--- tugas_tailwind/edit.php
+++ tugas_tailwind/edit.php
@@ -21,19 +21,19 @@ if(isset($_POST['submit'])){
 <?php include('./layouts/header.php');?>
 
 <!-- content -->
-            <div class="bg-grey-200 rounded-xl p-4">
+           <div class="bg-grey-200 rounded-xl p-4">
                 <h1 class="text-3xl mb-2">Form Input Nilai</h1>
                 <form action="" method="post">
-                    <div class="mb-3">
-                        <label for="nama">Nama</label>
-                        <input value="<?= $student['name']?>" type="text" class="rounded-xl p-3 block w-full" name="name" id="nama" placeholder="Masukan Nama Anda">
-                        <label for="nis">NIS</label>
-                        <input value="<?= $student['nis']?>" class="rounded-xl p-3 block w-full" type="number" name="nis" id="nis" placeholder="Masukan NIS Anda">
+                    <div class="mb-3 bg-gray-500 rounded-lg p-4 m-3">
+                        <label for="nama" class="text-white">Nama</label>
+                        <input value="<?= $student['name']?>" type="text" class="rounded-lg p-3 block w-full" name="name" id="nama" placeholder="Masukan Nama Anda">
+                        <label for="nis" class="text-white">NIS</label>
+                        <input value="<?= $student['nis']?>" class="rounded-lg p-3 block w-full" type="number" name="nis" id="nis" placeholder="Masukan NIS Anda">
                         <input type="hidden" name="id" value="<?= $student['id']?>">
+                        <div class="grid gap-2">
+                    <button name="submit" class="bg-blue-900 hover:bg-blue-700 p-3 m-4 rounded-lg text-white" type="submit">Submit</button>
+                        </div>
                     </div>
-                    <div class="grid gap-2">
-                    <button name="submit" class="bg-grey-600 hover:bg-grey-800 p-4 rounded-xl text-white" type="submit">Submit</button>
-                </div>
                 </form>
             </div>
 <?php include('./layouts/footer.php');?>
