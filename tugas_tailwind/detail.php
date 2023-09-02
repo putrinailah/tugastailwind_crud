42ffa16c60be059e860e240015308a9281e1cc5e
PIKACHA <140232108+putrinailah@users.noreply.github.com>
2023-09-02 11:47:02 +0700

    detail.php

--- tugas_tailwind/detail.php
+++ tugas_tailwind/detail.php
@@ -9,7 +9,7 @@ $student = Student::show($id);
 <?php include('./layouts/top.php'); ?>
 <?php include('./layouts/header.php'); ?>
 <!-- content -->
-<div class="flex bg-slate-300 rounded-xl p-3 m-3">
+<div class="flex bg-gray-400 rounded-xl p-3 m-3">
     <div class="basis-2/5">
         <p class="font-bold">nama</p>
         <p class="font-bold">nis</p>
@@ -20,7 +20,7 @@ $student = Student::show($id);
     </div>
 </div>
     <div class="grid gap-2">
-        <a href="index.php" class="bg-blue-500 p-3 rounded-xl text-white m-3 text-center">kembali</a>
+        <a href="index.php" class="bg-blue-800 hover:bg-blue-700 p-3 rounded-xl text-white m-3 text-center">kembali</a>
     </div>
 <?php include('./layouts/footer.php'); ?>
 <?php include('./layouts/bottom.php'); ?>
